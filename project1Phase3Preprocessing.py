b3fae2e1b3b0382c9366d3b3951074baf017212a
liamcampbell-png <liamc0004@gmail.com>
2024-08-26 11:19:36 -0500

    Update project1Phase3Preprocessing.py

--- project1Phase3Preprocessing.py
+++ project1Phase3Preprocessing.py
@@ -1,23 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 31 17:12:30 2024
 
-@author: Sriram Pemmaraju
-"""
 def makeNeighborLists(wordList):
     n = len(wordList)
     
-    # Create and initialize a list containing n adjacency lists
     adjList = []
     for i in range(n):
         adjList.append([])
 
 
-    # Go through every pair of words in wordList, check if they are adjacent
-    # and if they are, then set the appropriate slot in M to 1
-    #
-    # This is implemented using nested for-loops.
+
     for i in range(n):
         for j in range(n):
             if areNeighbors(wordList[i], wordList[j]):
@@ -26,11 +16,7 @@ def makeNeighborLists(wordList):
     return adjList
 
 def areNeighbors(w1, w2):
-    # Algorithm: Walk down both strings w1 and w2 and count the number of indices 
-    # at which w1 and w2 have distinct characters. If this count <= 1, then the function
-    # should return True; otherwise False
-    
-    # This algorithm can be implemented using a single for-loop
+  
     count = 0
     for i in range(len(w1)):
         count = count + (w1[i] != w2[i])
@@ -82,40 +68,6 @@ def binarySearch(L, w, first, last):
 def getIndex(L, w):
     return binarySearch(L, w, 0, len(L)-1)
     
-
-##################################################################
-#
-# Specification: The function reads words from the file "words.txt" and creates and
-# returns a list with these words. The words should in the same order in the list
-# as they appear in the file. Each string in the list of words should be exactly
-# 5 characters long.
-#
-# NEW: if the file word.txt is missing, this function should just return [] instead
-# of causing the program to cause an exception.
-#
-# Examples:
-# >>> L = readWords()
-# >>> len(L)
-# 5757
-# >>> L[len(L)-1]
-# 'zowie'
-# >>> L[0:10]
-# ['aargh',
-#  'abaca',
-#  'abaci',
-#  'aback',
-#  'abaft',
-#  'abase',
-#  'abash',
-#  'abate',
-#  'abbey',
-#  'abbot']
-# >>> L[1000]
-# 'coney'
-# >>> sorted(L)==L
-# True
-#
-###############################################################################
 def readWords():
     wordList = []
     try:
@@ -126,22 +78,7 @@ def readWords():
     except FileNotFoundError:
         print("File was not found")
     return wordList
-    
-###############################################################################     
-#
-# Specification:  This function takes a list of words and a list of file names.
-# It reads from each file in the given list of file names and extracts words from
-# the file. For each word in the list of words, it computes the frequency of this
-# word in all the files in the given list of file names. The function returns
-# the list of frequencies. The order in which frequencies appear in the frequency
-# list should match the order in which words appear in the given word list. In other
-# words, the frequency in slot 0 should be the frequency of smallerWordList[0],
-# the frequency in slot 1 should be the frequency of smallerWordList[1], etc.
-# The function should use "try and except" to gracefully deal with missing files.
-# If a file is missing, it should just skip over to the next file. If all files
-# are missing, then the frequency list returned should contain all 0's.
-#
-###############################################################################   
+       
 def computeFrequencies(smallerWordList, fileNameList):
     frequency = []
     fileList = []
@@ -160,28 +97,8 @@ def computeFrequencies(smallerWordList, fileNameList):
         frequency.append(freq)
     return frequency
     
-        
+    
 
-############################################################################### 
-# You can add as many other functions as you want to make your code streamlined,
-# readable, and efficient
-############################################################################### 
-
-
-############################################################################### 
-# main program starts here
-############################################################################### 
-
-# STEP 1: Identify the list of words in the largest connected component
-# (a) Read the list of all words in words.txt. Make sure that the 
-# program exits gracefully if words.txt is not available
-# (b) Build the adjacency list representation of the word network of this list of 
-# words
-# (c) Find all connected components of this word network
-# (d) Identify the largest connected component and create a list with the words 
-# in the largest connected component in sorted order
-#
-# Code for STEP 1 goes here
 wordList = readWords()
 
 if wordList != []:
@@ -200,16 +117,6 @@ if wordList != []:
         
 
 
-# STEP 2: Compute the frequencies of all the words in the largest connected component
-# and designate the p % of the words with highest frequency as "easy" words  
-# (a) Create a list containing all the names of text files downloaded from Project Gutenberg
-# (b) Call the function computeFrequencies to read from these files, extract words, and
-# update the frequencies of the words in the largest connected component  
-# (c) Read from the file parameters.txt to get the value of parameter p
-# (d) Designate the most frequent  p % of these words as "easy" words and the rest
-# as "hard" words 
-#
-# Code for STEP 2 goes here
 text_files = ["pg84.txt", "pg1342.txt", "pg2701.txt","pg2641.txt","pg37106.txt","pg64317.txt"]
 
 
@@ -241,15 +148,6 @@ for word in sortedWords:
         easyWords.append(word)
 
 
-# STEP 3: Write into the file gameInformation.txt
-# (a) Open the file "gameInformation.txt" for writing
-# (b) Write the number of easy words, followed by the easy words themselves in alphabetical order
-# (c) Write the number of hard words, followed by the hard words themselves in alphabetical order
-# (d) Write the adjacency list representation of the word network of the largest connected component
-# Make sure that everything is written into the file gameInformation.txt as per the specifications
-# in the project 1 handout
-#
-# Code for STEP 3 goes here
 sorted(easyWords)
 sorted(hardWords)
 adjList = makeNeighborLists(largeComponent)
